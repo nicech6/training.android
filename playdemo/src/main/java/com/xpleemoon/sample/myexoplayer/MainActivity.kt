b96aee70152a8e83583238e3f9ac624274e0443e
hai.cui <cuihai@qingting.fm>
2022-05-31 16:03:23 +0800

    修改部分重复依赖

--- playdemo/src/main/java/com/xpleemoon/sample/myexoplayer/MainActivity.kt
+++ playdemo/src/main/java/com/xpleemoon/sample/myexoplayer/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.xpleemoon.sample.myexoplayer
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.DefaultLoadControl
+import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.PlaybackPreparer
 import com.google.android.exoplayer2.source.ConcatenatingMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.ui.DefaultTimeBar
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.ui.TimeBar
+import com.google.android.exoplayer2.ui.TimeBar.OnScrubListener
 import com.google.android.exoplayer2.upstream.RawResourceDataSource
 import com.xpleemoon.player.AudioPlayer
+import com.xpleemoon.player.exception.PlaybackException
+import com.xpleemoon.player.listener.PlaybackListener
+import com.xpleemoon.player.listener.SimplePlaybackListener
+import com.xpleemoon.sample.myexoplayer.playercontroller.PlaybackState
 
 class MainActivity : AppCompatActivity() {
     private val audioPlayer by lazy {
@@ -15,24 +28,33 @@ class MainActivity : AppCompatActivity() {
             it.startEventLogger()
         }
     }
+
     /**
      * flv貌似不能正常播
      */
     private val sources = mapOf(
-                        "周星驰笑声本地asset" to "asset:///周星驰.mp3",
-            "新闻联播本地raw" to RawResourceDataSource.buildRawResourceUri(R.raw.cctv).toString(),
-            "钢琴-故事的开始" to "https://img.tukuppt.com/preview_music/00/08/60/preview-5b835eb33f4f15003.mp3",
+        "钢琴-故事的开始" to "https://img.tukuppt.com/preview_music/00/08/60/preview-5b835eb33f4f15003.mp3",
+        "周星驰笑声本地asset" to "asset:///周星驰.mp3",
+        "新闻联播本地raw" to RawResourceDataSource.buildRawResourceUri(R.raw.cctv).toString(),
     )
 
     private var debugView: TextView? = null
+    private var timeBar: DefaultTimeBar? = null
+    private var exoPlay: PlayerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         debugView = findViewById(R.id.debug_view)
+        timeBar = findViewById(R.id.timeBar)
+        exoPlay = findViewById(R.id.exo_play)
+        val uri = Uri.parse("https://cdn.xxxxxx.com/new-sing/66c3d05eaa177e07d57465f948f0d8b934b7a7ba.mp4")
+        val player = ExoPlayerFactory.newSimpleInstance(this,
+            DefaultTrackSelector(), DefaultLoadControl()
+        )
 
-        // 设置播放准备接口
+        exoPlay?.player = player
+
         audioPlayer.setPlaybackPreparer(PlaybackPreparer {
             // TODO 可用于播放页面的初始化
             val mediaSource = ConcatenatingMediaSource()
@@ -48,6 +70,43 @@ class MainActivity : AppCompatActivity() {
         }
         // 启动播放事件打印
         audioPlayer.startEventLogger()
+
+        val playbackListener: PlaybackListener = object : SimplePlaybackListener() {
+
+            override fun onPlaybackProgressChanged(
+                currentPositionMS: Long,
+                bufferedPositionMS: Long,
+                durationMS: Long
+            ) {
+                Log.i(
+                    "onPlaybackProgressChanged",
+                    "currentPositionMS" + currentPositionMS + "bufferedPositionMS" + bufferedPositionMS + "durationMS" + durationMS
+                )
+                timeBar?.setDuration(durationMS)
+                timeBar?.setBufferedPosition(bufferedPositionMS)
+                timeBar?.setPosition(currentPositionMS)
+            }
+
+            override fun onPlaybackStateChanged(playbackState: PlaybackState) {
+
+            }
+
+            override fun onPlayerError(error: PlaybackException) {
+
+            }
+        }
+        audioPlayer.addPlaybackListener(playbackListener)
+        timeBar?.addListener(object : OnScrubListener {
+            override fun onScrubStart(timeBar: TimeBar, position: Long) {
+
+            }
+
+            override fun onScrubMove(timeBar: TimeBar, position: Long) {
+                audioPlayer.seekTo(position)
+            }
+
+            override fun onScrubStop(timeBar: TimeBar, position: Long, canceled: Boolean) {}
+        })
     }
 
     fun onPlayControl(view: View) {
